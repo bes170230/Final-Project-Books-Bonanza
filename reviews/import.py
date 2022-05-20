2f47dacb06216292e3516b738c0779d507f7f17b
bes170230 <brad.e.stover@protonmail.com>
2022-05-20 01:11:43 -0600

    more finishing touches

--- reviews/import.py
+++ reviews/import.py
@@ -25,18 +25,12 @@ def import_books(csv_path):
         if header_line:
             header_line = False
             continue
-        print(f"Adding a book titled \"{title}\" by {author}.")
         book = Book(isbn=isbn, title=title, author=author, year=year)
         db.session.add(book)
         db.session.commit()
 
 def main():
-    print("Creating tables...")
     db.create_all()
-    print("Tables created!")
-    print("Importing books...")
-    import_books("books.csv")
-    print("Books imported!")
 
 if __name__ == "__main__":
     with app.app_context():
