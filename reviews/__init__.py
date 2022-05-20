fa8a03ce5f3458759b6642680432903fac169441
bes170230 <brad.e.stover@protonmail.com>
2022-05-20 01:26:24 -0600

    changed uri

--- reviews/__init__.py
+++ reviews/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecret'
 
 os.environ[
-        "DATABASE_URL"] = "postgresql+psycopg2://postgres:admin@127.0.0.1:5432/ranger_books_88"
+        "DATABASE_URL"] = "postgresql://tyuvmczxhkxerv:9ed58427a78a6c50b892d188a4c443cb92fe9fe14289a88b37feefa28c90f53e@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d5ogq7gr3o1ffa"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
--- reviews/import.py
+++ reviews/import.py
@@ -6,7 +6,7 @@ from models import Book
 from app import db
 
 os.environ[
-        "DATABASE_URL"] = "postgresql+psycopg2://postgres:admin@127.0.0.1:5432/ranger_books_88"
+        "DATABASE_URL"] = "postgresql://tyuvmczxhkxerv:9ed58427a78a6c50b892d188a4c443cb92fe9fe14289a88b37feefa28c90f53e@ec2-3-231-82-226.compute-1.amazonaws.com:5432/d5ogq7gr3o1ffa"
 
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
